0268447d46e5bd8eb3e8d131e7232d12a104da79
Dominik Karvonen <dominik1600@student.hamk.fi>
2018-10-06 15:05:23 +0300

    rautalanka

---
+++ Kotityot2/Kotityot2/Kotityot2/person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kotityot2
+{
+    public class Person
+    {
+        public string Name;
+        public Person(string _name)
+        {
+            Name = _name;
+        }
+    }
+}
